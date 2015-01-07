6c4d367a0a0a1532b3362c648e8e14507349690c
Carla kech <cocokechichian@gmail.com>
2015-01-06 21:30:00 -0800

    Finally finished day 1

---
+++ index.php
@@ -0,0 +1,133 @@
+<?php
+// THIS IS EXAMPLE 1 
+
+class Flower {
+	 //class body
+	 // class body
+	//class body
+}
+ class Phone{
+ 	//class body
+ 	//class body
+ 	//class body
+ }
+class Tv {
+	//class body
+	//class body
+	//class body
+}
+
+//THIS IS EXAMPLE 2
+
+$flower1 = newFlower();
+$flower2 = newFlower();
+
+$phone1 = newPhone();
+$phone2 = newPhone();
+
+$tv1 = newTv();
+$tv2 = newTv();
+
+//THIS IS EXAMPLE 3  
+class Flower {
+	public $brand = "daisy";
+	public $color = "yellow";
+	public $amount = "3";
+	public $price = 0;
+}
+class Phone {
+	public $brand = "iphone";
+	public $color = "white";
+	public $type = "iphone6";
+	public $price = 0;
+}
+class Tv {
+	public $brand = "samsung";
+	public $color = "black";
+	public $amount = "1";
+	public $price = 0;
+}
+
+// THIS IS EXAMPLE 4
+$Flower1 = new Flower();
+print $Flower1­>name;
+// random name 
+
+$Phone1 = new Phone();
+print $Phone1­>name;
+// random name 
+
+$Tv1 = new Tv();
+print $Tv1­>name;
+// random name 
+
+//THIS IS EXAMPLE 5
+public function myMethod($random, $class) {
+// comments
+}
+public function myMethod($daisy, $iphone) {
+// comments
+}
+
+public function myMethod($Tv, $samsung) {
+// comments
+}
+
+//THIS IS EXAMPLE 6
+
+class flower {
+	public $brand = "daisy";
+	public $color = "yellow";
+	public $amount = "3";
+	public $price = 0;
+
+	function getName() {
+	return “{$this­>brand}” .
+	“{$this­>color}”;
+	}
+}
+
+$Flower1 = new flower();
+$Flower1­>brand = “Daisy”;
+$Flower­>color = “yellow”;
+print “The flowers name is {Flower1­>getName()}.”;
+
+}
+
+class phone {
+	public $brand = "iphone";
+	public $color = "white";
+	public $type = "iphone6";
+	public $price = 0;
+
+	function getName() {
+	return “{$this­>brand}” .
+	“{$this­>color}”;
+	}
+}
+
+$iphone1-> = new phone();
+$iphone1­>brand = “iphone”;
+$iphone1­>color = “white”;
+print “The iphone is {iphone1­>getName()}.”;
+}
+
+class tv {
+	public $brand = "samsung";
+	public $color = "black";
+	public $amount = "1";
+	public $price = 0;
+
+	function getName() {
+	return “{$this­>brand}” .
+	“{$this­>color}”;
+	}
+}
+
+$tv1-> = new tv();
+$tv1­>brand = “samsung”;
+$tv1­>color = “black”;
+print “The Tv is {tv1­>getName()}.”;
+}
+
+?> 
